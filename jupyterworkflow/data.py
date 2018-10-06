d4bc7ff4e7d3931770d390b66e1f1da57db97ddb
ravivijay <reeves.skr@gmail.com>
2018-10-06 00:35:37 -0700

    Refactor for speed by inputting known format

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -20,9 +20,14 @@ def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL, force_download=Tru
     data: pandas.DF
         The fremont bridge data
     """
+    data = pd.read_csv('Fremont.csv',index_col='Date')
     if force_download or not os.path.exists(filename):
         urlretrieve(FREMONT_URL, 'Fremont.csv')
-    data = pd.read_csv('Fremont.csv',index_col='Date',parse_dates=True)
+    try:
+        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %H:%M:%S %p')
+    except TypeError:
+        data.index = pd.to_datetime(data.index)
+    
     data.columns = ['West', 'East']
     data['Total'] = data['East']+ data['West']
     return data
