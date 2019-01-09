e6adcdd9cbc70102c7435ea62dc460af7650b463
Ravi Sekar Vijayakumar <reeves.skr@gmail.com>
2019-01-08 18:05:27 -0800

    Delete utils.py

--- jupyterworkflow/utils.py
+++
@@ -1,8 +0,0 @@
-from slackclient import SlackClient
-def slack_message(message, channel):
-    token = 'xoxp-460216232545-461071818149-479586737393-48a351b055e1f1704759ef11fccb0843'
-    sc = SlackClient(token)
-    sc.api_call('chat.postMessage', channel=channel, 
-                text=message, username='reeves.skr',
-                icon_emoji=':robot_face:')
-
